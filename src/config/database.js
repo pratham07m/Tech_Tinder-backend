918e196116d0dca057972386f4b4316064f18dc5
Pratham_Mahant <118175804+pratham07m@users.noreply.github.com>
2025-02-05 13:35:59 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("enter a mongoDB connection URI");
+    await mongoose.connect("mongodb+srv://pratham2004m:pratham2004@cluster0.2qvwh.mongodb.net/devTinder");
 };
 
 module.exports = connectDB;
